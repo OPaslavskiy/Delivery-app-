b19a7c19072969513d751ccb78b19f92731f4707
OPaslavskiy <OPaslavskiy@users.noreply.github.com>
2023-05-25 20:22:52 +0000

    deploy: a6ac7377a3f40507e1cbd8c39cc356e0500fc841

---
+++ assets/ShoppingCart-5d814968.js
@@ -0,0 +1,106 @@
+import{i as e,j as t,r as l}from"./index-9114de9b.js";import{s as x}from"./submitOrder-01f4608a.js";const c=(i,r,s,o)=>{const{value:a}=i.target,p=s.map(n=>n.titel===r?{...n,quantity:a}:{...n,quantity:1}).filter(n=>n.quantity!=="0");o(p)},u=e.input`
+  width: 100%;
+  padding: 8px 12px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`,f=e.label`
+  display: block;
+  font-size: 16px;
+  text-transform: uppercase;
+`,g=({item:i,handleChange:r,contactInfo:s})=>t.jsxs(t.Fragment,{children:[t.jsx(f,{htmlFor:i,children:i}),t.jsx(u,{type:i==="email"?"email":"text",id:i,name:i,value:s[i],onChange:r,required:!0})]}),h=({contactInfo:i,setContactInfo:r})=>{const s=({target:{name:o,value:a}})=>{r(p=>({...p,[o]:a}))};return t.jsx(t.Fragment,{children:Object.keys(i).map(o=>t.jsx(g,{item:o,handleChange:s,contactInfo:i,setContactInfo:r},o))})},m=e.form`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  justify-content: flex-start;
+  border: 1px solid #5f4699;
+  border-radius: 8px;
+  padding: 28px;
+`,b=e.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 60px;
+  border: 1px solid #5f4699;
+  border-radius: 8px;
+  padding: 28px;
+  height: 560px;
+  overflow: auto;
+  width: 50%;
+`,j=e.div`
+  display: flex;
+  flex-wrap: wrap;
+
+  gap: 18px;
+  border: 1px solid #5f4699;
+  border-radius: 8px;
+  padding: 28px;
+  width: 50%;
+`,y=e.div`
+  display: flex;
+  gap: 28px;
+`,w=e.li`
+  height: 400px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  border: 1px solid #5f4699;
+  border-radius: 8px;
+  padding: 28px;
+`,C=e.button`
+  display: inline-block;
+  padding: 10px 20px;
+  margin-top: 12px;
+  margin-left: auto;
+  background-color: #368639;
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: bold;
+  text-decoration: none;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover,
+  &:focus {
+    background-color: #45a049;
+  }
+`,v=e.img`
+  width: 430px;
+  height: 320px;
+  object-fit: cover;
+  border-radius: 15px;
+`,k=e.p`
+  font-size: 28px;
+  margin-top: 12px;
+  margin-right: auto;
+`,z=e.div`
+  display: flex;
+  align-items: baseline;
+  margin-left: auto;
+  gap: 30px;
+`,I=e.input`
+  width: 160px;
+  padding: 5px 10px;
+  font-size: 24px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  text-align: center;
+`,S=e.div`
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+  margin-left: 18px;
+  gap: 30px;
+`,O=e.p`
+  font-size: 20px;
+`,q=e.p`
+  font-size: 22px;
+`,F=e.p`
+  font-size: 24px;
+  font-weight: bold;
+  color: #333333;
+  margin-bottom: 10px;
+  text-align: center;
+`,G=()=>{const i=JSON.parse(localStorage.getItem("basket"))||[],[r,s]=l.useState(i),[o,a]=l.useState({name:"",email:"",phone:"",address:""}),p=r.reduce((n,d)=>n+d.quantity*d.price,0);return t.jsxs(m,{children:[t.jsxs(y,{children:[r.length?t.jsx(j,{children:t.jsx(h,{contactInfo:o,setContactInfo:a})}):t.jsx(t.Fragment,{}),t.jsx(b,{children:r.length?r.map(n=>t.jsxs(w,{children:[t.jsx(v,{src:n.image,alt:"",width:250,height:180}),t.jsxs(S,{children:[t.jsx(k,{children:n.titel}),t.jsxs(O,{children:["Price: ",n.price," UAH"]}),t.jsx(I,{type:"number",min:"0",defaultValue:"1",id:`quantityOf${n.titel}`,onChange:d=>c(d,n.titel,r,s)})]})]},n._id)):t.jsx(F,{children:"The Cart is empty."})})]}),r.length?t.jsxs(z,{children:[t.jsxs(q,{children:["Total price: ",p," UAH"]}),t.jsx(C,{onClick:()=>{x(r,o,s,a,p)},children:"Submit"})]}):t.jsx(t.Fragment,{})]})};export{G as default};
---
+++ assets/Shops-920955b8.js
@@ -0,0 +1,96 @@
+import{i as e,r as l,j as o}from"./index-9114de9b.js";import{a as p,g as a}from"./submitOrder-01f4608a.js";const x=e.button`
+  width: 250px;
+  height: 120px;
+  text-transform: uppercase;
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #998787;
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: bold;
+  text-decoration: none;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  margin-top: 2px;
+
+  &:disabled {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
+
+  &:hover,
+  &:focus {
+    background-color: #45a049;
+  }
+`,u=({loadGoods:i})=>{const[n,c]=l.useState(null),r=["mcduck","theshaurma","elari","faina","sushiabw"],t=(s,d)=>{i(d),c(s)};return o.jsx("ul",{children:r.map((s,d)=>o.jsx("li",{children:o.jsx(x,{onClick:()=>{t(d,s)},disabled:n!==null&&n!==d,children:`${s}`})},s))})},f=e.div`
+  display: flex;
+  gap: 32px;
+  justify-content: flex-start;
+  border: 1px solid #5f4699;
+  border-radius: 8px;
+  padding: 28px;
+`,h=e.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid #5f4699;
+  border-radius: 8px;
+  padding: 18px;
+`,b=e.h2`
+  margin-bottom: 18px;
+`,m=e.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 60px;
+  border: 1px solid #5f4699;
+  border-radius: 8px;
+  padding: 28px;
+
+  height: 760px;
+  overflow: auto;
+`,g=e.li`
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid #5f4699;
+  border-radius: 8px;
+  padding: 28px;
+  display: flex;
+`,k=e.img`
+  width: 330px;
+  height: 220px;
+  object-fit: cover;
+  border-radius: 15px;
+`,j=e.p`
+  font-size: 28px;
+  margin-top: 12px;
+  margin-right: auto;
+`,w=e.button`
+  display: inline-block;
+  padding: 10px 20px;
+  margin-top: 12px;
+  margin-left: auto;
+  background-color: #368639;
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: bold;
+  text-decoration: none;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover,
+  &:focus {
+    background-color: #45a049;
+  }
+`,y=e.p`
+  font-size: 24px;
+  font-weight: bold;
+  color: #333333;
+  margin-bottom: 10px;
+  text-align: center;
+`,B=()=>{const[i,n]=l.useState([]);function c(r){switch(r){case"mcduck":a("mcduck").then(t=>{n(t)});break;case"theshaurma":a("theshaurma").then(t=>{n(t)});break;case"elari":a("elari").then(t=>{n(t)});break;case"faina":a("faina").then(t=>{n(t)});break;case"sushiabw":a("sushiabw").then(t=>{n(t)});break;default:return}}return o.jsxs(f,{children:[o.jsxs(h,{children:[o.jsx(b,{children:"Shops:"}),o.jsx(u,{loadGoods:c})]}),o.jsx(m,{children:i.length?i==null?void 0:i.map(r=>o.jsxs(g,{children:[o.jsx(k,{src:r.image,alt:r.titel}),o.jsx(j,{children:r.titel}),o.jsx(w,{type:"button",onClick:()=>p(r),children:"Add to Cart"})]},r._id)):o.jsx(y,{children:"Welcome! Choose a store to order delicious food."})})]})};export{B as default};
