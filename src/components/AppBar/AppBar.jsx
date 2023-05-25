35210e3b6068cdac6008ef12504661fcfe3f00bd
OPaslavskiy <bankir.op@gmail.com>
2023-05-25 20:09:40 +0300

    add style forf appBar

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,7 +2,7 @@ import { Header, LogoLink } from "./AppBar.styled";
 
 export const AppBar = () => {
   return (
-    <Header className="header">
+    <Header>
       <LogoLink to="/">Shop</LogoLink>
       <LogoLink to="/shopingcart">Shopping Cart</LogoLink>
     </Header>
--- src/components/AppBar/AppBar.styled.jsx
+++ src/components/AppBar/AppBar.styled.jsx
@@ -3,52 +3,30 @@ import { NavLink } from "react-router-dom";
 
 export const Header = styled.header`
   display: flex;
+  gap: 32px;
   justify-content: flex-start;
-  padding: 15px;
   padding-left: 64px;
   padding-right: 64px;
   background: linear-gradient(
     114.99deg,
-    #471ca9 -0.99%,
-    #5736a3 54.28%,
-    #4b2a99 78.99%
+    #c1b9d4 -0.99%,
+    #7158ac 54.28%,
+    #5f4699 78.99%
   );
-`;
-
-export const Logo = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 20px;
-`;
-
-export const SignIn = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-`;
-
-export const LinkSignIn = styled.a`
-  color: #fff;
-`;
-
-export const Link = styled(NavLink)`
-  color: #fff;
-  margin-left: 14px;
-  margin-right: 4px;
-  text-decoration: none;
 `;
 
 export const LogoLink = styled(NavLink)`
   display: flex;
-  font-family: "Montserrat";
   font-style: normal;
   align-items: center;
   color: #fff;
   text-decoration: none;
-  font-size: 32px;
+  font-size: 28px;
   font-weight: bold;
   margin-left: 14px;
   line-height: 1.2;
+  padding-top: 15px;
+  padding-bottom: 15px;
 
   &:hover,
   &:focus {
