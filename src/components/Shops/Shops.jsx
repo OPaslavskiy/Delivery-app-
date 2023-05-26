2d062f4868f99421ec7ba89128f0583f9e00b587
OPaslavskiy <bankir.op@gmail.com>
2023-05-26 10:12:09 +0300

    add spiner

--- src/components/Shops/Shops.jsx
+++ src/components/Shops/Shops.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { getGoods, addToBasket } from "../../services";
 import ButtonList from "../ButtonList/ButtonList";
+import { Loader } from "../Loader/Loader";
 import {
   Conteiner,
   ShopsItem,
@@ -15,31 +16,38 @@ import {
 
 const Shops = () => {
   const [goods, setGoods] = useState([]);
+  const [status, setStatus] = useState("stoped");
 
   function loadGoods(operation) {
+    setStatus("pending");
     switch (operation) {
       case "mcduck":
         getGoods("mcduck").then((res) => {
+          setStatus("resolved");
           setGoods(res);
         });
         break;
       case "theshaurma":
         getGoods("theshaurma").then((res) => {
+          setStatus("resolved");
           setGoods(res);
         });
         break;
       case "elari":
         getGoods("elari").then((res) => {
+          setStatus("resolved");
           setGoods(res);
         });
         break;
       case "faina":
         getGoods("faina").then((res) => {
+          setStatus("resolved");
           setGoods(res);
         });
         break;
       case "sushiabw":
         getGoods("sushiabw").then((res) => {
+          setStatus("resolved");
           setGoods(res);
         });
         break;
@@ -54,21 +62,27 @@ const Shops = () => {
         <TitleShops>Shops:</TitleShops>
         <ButtonList loadGoods={loadGoods} />
       </ShopsItem>
-      <GoodsList>
-        {goods.length ? (
-          goods?.map((good) => (
-            <GoodItem key={good._id}>
-              <Image src={good.image} alt={good.titel} />
-              <GoodName>{good.titel}</GoodName>
-              <GoodButton type="button" onClick={() => addToBasket(good)}>
-                Add to Cart
-              </GoodButton>
-            </GoodItem>
-          ))
-        ) : (
-          <Greeting>Welcome! Choose a store to order delicious food.</Greeting>
-        )}
-      </GoodsList>
+      {status === "pending" ? (
+        <Loader />
+      ) : (
+        <GoodsList>
+          {goods.length ? (
+            goods?.map((good) => (
+              <GoodItem key={good._id}>
+                <Image src={good.image} alt={good.titel} />
+                <GoodName>{good.titel}</GoodName>
+                <GoodButton type="button" onClick={() => addToBasket(good)}>
+                  Add to Cart
+                </GoodButton>
+              </GoodItem>
+            ))
+          ) : (
+            <Greeting>
+              Welcome! Choose a store to order delicious food.
+            </Greeting>
+          )}
+        </GoodsList>
+      )}
     </Conteiner>
   );
 };
